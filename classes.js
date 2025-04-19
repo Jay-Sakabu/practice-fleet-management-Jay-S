fb8cf0bba28817a04aca4743d469cda4eed5cee3
Jay S <js4cap@gmail.com>
2025-04-19 12:44:08 -0500

    Initial Commit

---
+++ classes.js
@@ -0,0 +1,34 @@
+class Vehicles
+{
+    constructor(type, make, model, year, mileage)
+    {
+        this.type = type;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+    }
+
+    drive(distance)
+    {
+        return this.mileage += distance;
+    }
+
+    getDetails()
+    {
+        console.log(`Selected vehicle is a ${this.type} of make & model ${this.make} ${this.model} with a year of ${this.year} and mileage at: ${this.mileage}`);
+    }
+}
+
+let car = new Vehicles("Car", "Hyundai", "Sonata", 2018, 5000);
+let truck = new Vehicles("Truck", "Mitsubishi", "Monster", 1892, 17000);
+let motorcycle = new Vehicles("Motorcycle", "Polaris", "Slingshot", 1738, 105000);
+
+
+car.drive(700);
+truck.drive(900);
+motorcycle.drive(1700);
+
+car.getDetails();
+truck.getDetails();
+motorcycle.getDetails();
